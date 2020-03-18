da8ef6ee19e85bca3813cd188857a712ac45126b
webmontana <magarampage@gmail.com>
2020-03-18 14:27:39 +0300

    SCRUM-5464 width

--- f-seo-agi.php
+++ f-seo-agi.php
@@ -3,11 +3,11 @@
 Plugin Name: F-Seo Auto Google Images
 Description: Плагин автоматической подгрузки картинок из Google Images в текстовый редактор WordPress
 Author: F-Seo
-Version: 2.6
+Version: 2.7
 Author URI: http://f-seo.ru/
 */
 
-define('FSEO_AGI_CURRENT_VERSION', '2.6');
+define('FSEO_AGI_CURRENT_VERSION', '2.7');
 
 include __DIR__ . '/AgiGoogleImage.php';
 
